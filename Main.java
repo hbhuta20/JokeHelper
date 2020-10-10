f686a5652931f1a0c39b478705961399465d02a0
Hetansh Bhuta <hetansh.bhuta@patelhs.org>
2020-10-10 15:01:06 +0000

    Added scanner to read intergers and strings. Added while loop to create multiple jokes.

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Let's make a joke!");
+    Scanner sc= new Scanner(System.in);
+    System.out.println("How many jokes do you want? (number)");
+
+    int x = sc.nextInt();
+    sc.nextLine();
+    
+    while(x>0){
+      System.out.println("Give me an adjective");
+      String adjective = sc.nextLine();
+      System.out.println("Yo Mamma is "+adjective);
+      x--;
+    }
   }
 }
