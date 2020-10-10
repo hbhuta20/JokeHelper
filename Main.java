e0acd9cad56090db46533e17a35ec6b519fc2230
Hetansh Bhuta <hetansh.bhuta@patelhs.org>
2020-10-10 15:29:41 +0000

    I changes the while into for loop. I then added if-else statements with boolean conditions.

--- Main.java
+++ Main.java
@@ -3,16 +3,23 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Let's make a joke!");
     Scanner sc= new Scanner(System.in);
-    System.out.println("How many jokes do you want? (number)");
+    System.out.println("How many nice jokes do you want? (number)");
 
-    int x = sc.nextInt();
+    int numJokes = sc.nextInt();
     sc.nextLine();
-    
-    while(x>0){
+
+    for(int x=0; x < numJokes; x++){
       System.out.println("Give me an adjective");
       String adjective = sc.nextLine();
-      System.out.println("Yo Mamma is "+adjective);
-      x--;
+      if(adjective != "" || (numJokes > 2 && numJokes < 4)){
+         System.out.println("Yo Mamma is " + adjective);
+      } 
+      else{
+        System.out.println("Yo Mamma is not " + adjective);
+      }
+     
     }
+
+
   }
 }
